500c6c90d5ff32894924d0b0e10c5f351d9659e9
Simon Walker <simonw@nait.ca>
2024-08-20 06:47:36 -0600

    added pcf8551 stuff

--- PCF8551.c
+++ PCF8551.c
@@ -2,8 +2,13 @@
 // Simon Walker, NAIT
 
 #include <avr/io.h>
+#include <avr/pgmspace.h> // definitions to place items in flash
 #include "I2C.h"
 #include "PCF8551.h"
+
+// debug
+#include <stdio.h>
+#include "sci.h"
 
 #define PCF8551_ADDR 0x38
 
@@ -17,6 +22,11 @@
 */
 
 // private helpers
+
+// memory buffer for display data (written at once)
+unsigned char PCF8551_DispRAM [20] = {0}; // offset to address 4 in device
+
+
 int PCF8551_Write (unsigned char ucAddr, unsigned char ucData)
 {
 	if (I2C_Start(PCF8551_ADDR, I2C_WRITE))
@@ -27,6 +37,26 @@ int PCF8551_Write (unsigned char ucAddr, unsigned char ucData)
 
 	if (I2C_Write8(ucData, I2C_STOP))
 	return -3;
+	
+	return 0;
+}
+
+int PCF8551_WriteRAM (void)
+{
+	if (I2C_Start(PCF8551_ADDR, I2C_WRITE))
+	return -1;
+
+	if (I2C_Write8(0x04, I2C_NOSTOP))
+	return -2;
+	
+	for (int i = 0; i < 19; ++i)
+	{
+		if (I2C_Write8(PCF8551_DispRAM[i], I2C_NOSTOP))
+		return -3;
+	}
+
+	if (I2C_Write8(PCF8551_DispRAM[19], I2C_STOP))
+	return -4;
 	
 	return 0;
 }
@@ -42,11 +72,17 @@ int PCF8551_Read (unsigned char * Target)
 	return 0;
 }
 
+// public functions
 int PCF8551_Init (void)
 {	
 	PCF8551_Write(0x01, 0b00000100); // device control, osc on, 64Hz COE off
 	PCF8551_Write(0x02, 0b00000000); // defaults for display control (off)
-	PCF8551_Write(0x03, 0b00000000); // display control, no blink
+	PCF8551_Write(0x03, 0b00000000); // display control, no blink		
+	
+	for (int i = 0; i < 20; ++i)	
+	PCF8551_DispRAM[i] = 0;
+	
+	PCF8551_WriteRAM();
 	
 	return 0;		
 }
@@ -61,27 +97,225 @@ void PCF8551_DisplayOff (void)
 	PCF8551_Write(0x02, 0b00000000); // display off
 }
 
+typedef struct PCF8551_SegMap
+{
+	unsigned char reg;
+	unsigned char bit;	
+} PCF8551_SegMap;
+
+const PCF8551_SegMap _SegLookup [16 * 8] =
+{
+	// pos 0
+	{ 0x15, 2 }, // A
+	{ 0x10, 2 }, // B
+	{ 0x0b, 2 }, // C
+	{ 0x04, 1 }, // D
+	{ 0x09, 1 }, // E
+	{ 0x0E, 1 }, // F
+	{ 0x10, 1 }, // G
+	{ 0x15, 1 }, // H		
+	{ 0x13, 2 }, // I				
+	{ 0x0E, 2 }, // J						
+	{ 0x09, 2 }, // K
+	{ 0x04, 2 }, // L
+	{ 0x06, 1 }, // M
+	{ 0x0b, 1 }, // N
+	{ 0x06, 2 }, // DP
+	{ 0x13, 1 }, // CA
+	// pos 1
+	{ 0x15, 8 }, // A
+	{ 0x10, 8 }, // B
+	{ 0x0b, 8 }, // C
+	{ 0x04, 4 }, // D
+	{ 0x09, 4 }, // E
+	{ 0x0E, 4 }, // F
+	{ 0x10, 4 }, // G
+	{ 0x15, 4 }, // H
+	{ 0x13, 8 }, // I
+	{ 0x0E, 8 }, // J
+	{ 0x09, 8 }, // K
+	{ 0x04, 8 }, // L
+	{ 0x06, 4 }, // M
+	{ 0x0b, 4 }, // N
+	{ 0x06, 8 }, // DP
+	{ 0x13, 4 }, // CA
+	// pos 2
+	{ 0x15, 32 }, // A
+	{ 0x10, 32 }, // B
+	{ 0x0b, 32 }, // C
+	{ 0x04, 16 }, // D
+	{ 0x09, 16 }, // E
+	{ 0x0E, 16 }, // F
+	{ 0x10, 16 }, // G
+	{ 0x15, 16 }, // H
+	{ 0x13, 32 }, // I
+	{ 0x0E, 32 }, // J
+	{ 0x09, 32 }, // K
+	{ 0x04, 32 }, // L
+	{ 0x06, 16 }, // M
+	{ 0x0b, 16 }, // N
+	{ 0x06, 32 }, // DP
+	{ 0x13, 16 }, // CA
+	// pos 3
+	{ 0x15, 128 }, // A
+	{ 0x10, 128 }, // B
+	{ 0x0b, 128 }, // C
+	{ 0x04, 64 }, // D
+	{ 0x09, 64 }, // E
+	{ 0x0E, 64 }, // F
+	{ 0x10, 64 }, // G
+	{ 0x15, 64 }, // H
+	{ 0x13, 128 }, // I
+	{ 0x0E, 128 }, // J
+	{ 0x09, 128 }, // K
+	{ 0x04, 128 }, // L
+	{ 0x06, 64 }, // M
+	{ 0x0b, 64 }, // N
+	{ 0x06, 128 }, // DP
+	{ 0x13, 64 }, // CA
+	// pos 4
+	{ 0x16, 2 }, // A
+	{ 0x11, 2 }, // B
+	{ 0x0C, 2 }, // C
+	{ 0x05, 1 }, // D
+	{ 0x0A, 1 }, // E
+	{ 0x0F, 1 }, // F
+	{ 0x11, 1 }, // G
+	{ 0x16, 1 }, // H
+	{ 0x14, 2 }, // I
+	{ 0x0F, 2 }, // J
+	{ 0x0A, 2 }, // K
+	{ 0x05, 2 }, // L
+	{ 0x07, 1 }, // M
+	{ 0x0C, 1 }, // N
+	{ 0x07, 2 }, // DP
+	{ 0x14, 1 }, // CA
+	// pos 5
+	{ 0x16, 8 }, // A
+	{ 0x11, 8 }, // B
+	{ 0x0C, 8 }, // C
+	{ 0x05, 4 }, // D
+	{ 0x0A, 4 }, // E
+	{ 0x0F, 4 }, // F
+	{ 0x11, 4 }, // G
+	{ 0x16, 4 }, // H
+	{ 0x14, 8 }, // I
+	{ 0x0F, 8 }, // J
+	{ 0x0A, 8 }, // K
+	{ 0x05, 8 }, // L
+	{ 0x07, 4 }, // M
+	{ 0x0C, 4 }, // N
+	{ 0x07, 8 }, // DP
+	{ 0x14, 4 }, // CA
+	// pos 6
+	{ 0x16, 32 }, // A
+	{ 0x11, 32 }, // B
+	{ 0x0C, 32 }, // C
+	{ 0x05, 16 }, // D
+	{ 0x0A, 16 }, // E
+	{ 0x0F, 16 }, // F
+	{ 0x11, 16 }, // G
+	{ 0x16, 16 }, // H
+	{ 0x14, 32 }, // I
+	{ 0x0F, 32 }, // J
+	{ 0x0A, 32 }, // K
+	{ 0x05, 32 }, // L
+	{ 0x07, 16 }, // M
+	{ 0x0C, 16 }, // N
+	{ 0x07, 32 }, // DP
+	{ 0x14, 16 }, // CA
+	// pos 7
+	{ 0x16, 128 }, // A
+	{ 0x11, 128 }, // B
+	{ 0x0C, 128 }, // C
+	{ 0x05, 64 }, // D
+	{ 0x0A, 64 }, // E
+	{ 0x0F, 64 }, // F
+	{ 0x11, 64 }, // G
+	{ 0x16, 64 }, // H
+	{ 0x14, 128 }, // I
+	{ 0x0F, 128 }, // J
+	{ 0x0A, 128 }, // K
+	{ 0x05, 128 }, // L
+	{ 0x07, 64 }, // M
+	{ 0x0C, 64 }, // N
+	{ 0x07, 128 }, // DP
+	{ 0x14, 64 } // CA				
+};
+
+void PCF8551_SegTog (int iPos, PCF8551_Seg seg)
+{
+	// map individual segment at desired position to memory array
+	// since the mapping is quite arbitrary, this will help with higher-level functions
+	// positions are 0-7 as left to right address on display, segs from datasheet
+	// this toggles the segment, and does not alter any other bits
+	if (iPos < 0 || iPos > 7)
+		return;
+		
+	// find pointer into the lookup table
+	int iOffset = iPos * 16 + seg;
+	
+	// grab the bit that applies for this segment/position
+	PCF8551_SegMap data = _SegLookup[iOffset];
+	
+	// toggle that bit in the display image, normalizing array/register position
+	PCF8551_DispRAM[data.reg - 4] ^= data.bit;	
+}
+
 void PCF8551_Test (void)
 {
-	PCF8551_Write(0x04, 0b00000000); // random segs
-	PCF8551_Write(0x05, 0b00000000); // random segs
-	PCF8551_Write(0x06, 0b00000000); // random segs
-	PCF8551_Write(0x07, 0b00000000); // random segs
-	PCF8551_Write(0x08, 0b00000000); // random segs
-	PCF8551_Write(0x09, 0b00000000); // random segs
-	PCF8551_Write(0x0a, 0b00000000); // random segs
-	PCF8551_Write(0x0b, 0b00000000); // random segs
-	PCF8551_Write(0x0c, 0b00000000); // random segs
-	PCF8551_Write(0x0d, 0b00000000); // random segs
-	PCF8551_Write(0x0e, 0b00000000); // random segs
-	PCF8551_Write(0x0f, 0b00000000); // random segs
-	PCF8551_Write(0x10, 0b00000000); // random segs
-	PCF8551_Write(0x11, 0b00000000); // random segs
-	PCF8551_Write(0x12, 0b00000000); // random segs
-	PCF8551_Write(0x13, 0b00000000); // random segs
-	PCF8551_Write(0x14, 0b00000000); // random segs
-	PCF8551_Write(0x15, 0b00000000); // random segs
-	PCF8551_Write(0x16, 0b00000000); // random segs	
+	//PCF8551_SegOn (0, SEG_A);
+	//PCF8551_SegOn (0, SEG_D);
+	//PCF8551_SegOn (0, SEG_B);
+	//PCF8551_SegOn (0, SEG_C);
+	//PCF8551_SegOn (0, SEG_I);
+	//PCF8551_SegOn (0, SEG_K);
+	//PCF8551_SegOn (0, SEG_M);
+//
+	//PCF8551_SegOn (1, SEG_A);
+	//PCF8551_SegOn (1, SEG_D);
+	//PCF8551_SegOn (1, SEG_F);
+	//PCF8551_SegOn (1, SEG_E);
+	//PCF8551_SegOn (1, SEG_B);
+	//PCF8551_SegOn (1, SEG_C);	
+//
+	//PCF8551_SegOn (2, SEG_A);
+	//PCF8551_SegOn (2, SEG_D);
+	//PCF8551_SegOn (2, SEG_F);
+	//PCF8551_SegOn (2, SEG_E);
+	//PCF8551_SegOn (2, SEG_B);
+	//PCF8551_SegOn (2, SEG_C);
+//
+	//PCF8551_SegOn (3, SEG_A);
+	//PCF8551_SegOn (3, SEG_D);
+	//PCF8551_SegOn (3, SEG_B);
+	//PCF8551_SegOn (3, SEG_C);
+	//PCF8551_SegOn (3, SEG_I);
+	//PCF8551_SegOn (3, SEG_K);
+	//PCF8551_SegOn (3, SEG_M);
+//
+	//PCF8551_SegOn (4, SEG_A);
+	//PCF8551_SegOn (4, SEG_D);
+	//PCF8551_SegOn (4, SEG_J);
+	//PCF8551_SegOn (4, SEG_N);
+	
+	PCF8551_SegTog (2, SEG_A);
+	PCF8551_SegTog (2, SEG_F);
+	PCF8551_SegTog (2, SEG_E);
+	PCF8551_SegTog (2, SEG_D);
+
+	PCF8551_SegTog (3, SEG_E);
+	PCF8551_SegTog (3, SEG_F);
+	PCF8551_SegTog (3, SEG_H);
+	PCF8551_SegTog (3, SEG_L);
+	PCF8551_SegTog (3, SEG_B);
+	PCF8551_SegTog (3, SEG_C);
+	
+	PCF8551_SegTog (4, SEG_A);
+	PCF8551_SegTog (4, SEG_I);
+	PCF8551_SegTog (4, SEG_M);
+	
+	PCF8551_WriteRAM();	
 }
 
 
--- PCF8551.h
+++ PCF8551.h
@@ -6,7 +6,16 @@
 //int PCF8551_Write (unsigned char ucAddr, unsigned char ucData);
 //int PCF8551_Read (unsigned char * Target);
 
+typedef enum PCF8551_Seg
+{
+	SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G, SEG_H, SEG_I, SEG_J, SEG_K, SEG_L, SEG_M, SEG_N, SEG_DP, SEG_CA  
+} PCF8551_Seg;
+
 int PCF8551_Init (void);
 void PCF8551_DisplayOn (void);
 void PCF8551_DisplayOff (void);
+void PCF8551_SegTog (int iPos, PCF8551_Seg seg);
+int PCF8551_WriteRAM (void);
+
+
 void PCF8551_Test (void);
